1d8f94a2b8d010a462cb2671c937619fbe027a4e
F_Piacacho <2314008827@qq.com>
2019-08-08 16:56:44 +0800

    pass

--- 求元素在页面的偏移量/js/main.js
+++ 求元素在页面的偏移量/js/main.js
@@ -2,4 +2,23 @@ window.onload = function(){
     function $(id){
         return typeof id ==='string'?document.getElementById(id):null;
     }
+    
+    console.log(getElementLeft($('box4')));
+    function getElementLeft(obj){
+        //1.获取当前元素左偏移量
+        var actuaiLeft = obj.offsetLeft;
+        // return actuaiLeft;
+        //2.求出定位父级
+        var parent = obj.offsetParent; //返回目标元素最近的定位父级
+        //3.循环
+        while(parent != null){
+            //求出当前左方偏移量
+            actuaiLeft = actuaiLeft + parent.clientLeft + parent.offsetLeft;
+            //2更新定位父级
+            console.log(parent);
+            parent = parent.offsetParent;
+        }
+
+        return actuaiLeft + 'px';
+    }
 }
