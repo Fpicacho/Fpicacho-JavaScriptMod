e43e2c38aaaf0a9cc1140aea529a535ad17aac5e
F_Piacacho <2314008827@qq.com>
2019-08-08 17:02:13 +0800

    TOP

--- 求元素在页面的偏移量/js/main.js
+++ 求元素在页面的偏移量/js/main.js
@@ -4,6 +4,7 @@ window.onload = function(){
     }
     
     console.log(getElementLeft($('box4')));
+    console.log(getElementTop($('box4')));
     function getElementLeft(obj){
         //1.获取当前元素左偏移量
         var actuaiLeft = obj.offsetLeft;
@@ -20,5 +21,20 @@ window.onload = function(){
         }
 
         return actuaiLeft + 'px';
+    }
+
+    function getElementTop(obj){
+        var actuaiTop = obj.offsetTop;
+        var parent = obj.offsetParent; //返回目标元素最近的定位父级
+        //3.循环
+        while(parent != null){
+            //求出当前左方偏移量
+            actuaiLeft = actuaiTop + parent.clientTop + parent.offsetTop;
+            //2更新定位父级
+            console.log(parent);
+            parent = parent.offsetParent;
+        }
+
+        return actuaiTop + 'px';
     }
 }
