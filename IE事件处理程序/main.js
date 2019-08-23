d3a1c214304275fba084a594ce2c3ed1625e615f
F_Piacacho <2314008827@qq.com>
2019-08-24 03:59:09 +0800

    ie

---
+++ IE事件处理程序/main.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    // IE:attachEvenet() detachEvenet()
+    var box = document.getElementById('box');
+    box.attachEvent('onclick',function(){
+        //ie中this指向window
+        // this.innerHTML += '1';  与DOM2类似 支持绑定多个事件
+        box.innerHTML += '1';
+    })
+
+    // box.detachEvent('事件名'，事件处理程序);
+}
